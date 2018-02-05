492245c6b59642b5797bc35d520a69be74c5b441
ShyAlly <1447610+ShyAlly@users.noreply.github.com>
2018-02-05 00:00:10 -0500

    Initial test

---
+++ src/CardType.ts
@@ -0,0 +1,19 @@
+enum CardType {
+
+    Evo = 0,
+    Balanced = 1,
+    Physical = 2,
+    Healer = 3,
+    Dragon = 4,
+    God = 5,
+    Attacker = 6,
+    Devil = 7,
+    Machine = 8,
+    UNKNOWN9 = 9,
+    UNKNOWN10 = 10,
+    UNKNOWN11 = 11,
+    Awakening = 12,
+    UNKNOWN13 = 13,
+    Enhance = 14,
+    Redeemable = 15
+}
